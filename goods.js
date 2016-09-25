82b31803932c7b803e30c506668570c0c7e2bf37
Mikkel Kirkgaard Nielsen <memb_github@mikini.dk>
2016-09-25 12:50:50 +0200

    Start work on order form
    *Load and show goods availability based on week number
    *Open/close order on the decided weekdays

---
+++ goods.js
@@ -0,0 +1,15 @@
+var goods=[
+    {"week":38,
+     "items":[
+	 {"name":"Grøntsagspose","unit":"poser","field":"grøntpose"}
+     ]
+    },
+    {"week":40,
+     "items":[
+	 {"name":"Vårrug, 2 kg ","unit":"poser","field":"vaarrug", "price":44}
+	,{"name":"Æg, 6 stk","unit":"bakker","field":"6_æg", "price":20}
+	,{"name":"Ølandshvede, 1 kg","unit":"poser","field":"ølandshvede_1kg", "price":22}
+	,{"name":"Kartofler","field":"kartofler","unit":"kg.", "price":10}
+     ]
+    }    
+];
