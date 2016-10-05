0a0146244eb840cdad7b11e06d10253c667787f4
Mikkel Kirkgaard Nielsen <miki@vestergaardcompany.com>
2016-10-05 02:09:15 +0200

    More work on order functionality
    *Improve calculation of delivery date
    *Show exact dates of delivery and order open/close
    *Add possibility of pretending a date for testing by get params
    *Add total price calculation to order form
    *Validate order before submit (check valid member # and items selected)
    *Support a group type in goods definition to separate items
    *Add week 42 goods definitions
    *Remove manual order information
    *Add info about new order procedure

--- goods.js
+++ goods.js
@@ -3,13 +3,39 @@ var goods=[
      "items":[
 	 {"name":"Grøntsagspose","unit":"poser","field":"grøntpose"}
      ]
-    },
-    {"week":40,
+    }
+    ,{"week":40,
      "items":[
 	 {"name":"Vårrug, 2 kg ","unit":"poser","field":"vaarrug", "price":44}
 	,{"name":"Æg, 6 stk","unit":"bakker","field":"6_æg", "price":20}
 	,{"name":"Ølandshvede, 1 kg","unit":"poser","field":"ølandshvede_1kg", "price":22}
 	,{"name":"Kartofler","field":"kartofler","unit":"kg.", "price":10}
      ]
-    }    
+    }
+    ,{"week":42,
+     "items":[
+	 {"group":"Grøntsager"}
+	,{"name":"Kartofler","unit":"kg.", "price":9}
+	,{"name":"Gulerødder","unit":"kg.", "price":9}
+	,{"name":"Pastinakker","unit":"kg.", "price":9}
+	,{"name":"Løg","unit":"kg.", "price":9}
+	,{"name":"Rødbeder","unit":"kg.", "price":9}
+	,{"name":"Gule rødbeder","unit":"kg.", "price":9}
+	,{"name":"Stribede rødbeder","unit":"kg.", "price":9}
+	,{"name":"Persille","unit":"bundt.", "price":10}
+	,{"name":"Græskar","unit":"stk.", "price":30}
+        ,{"group":"Æg", "description":"Æggene er størrelse M/L, er gennemlyste og datostemplet som reglerne foreskriver ved detailsalg. Køber man som privat direkte fra producenten er reglerne mindre restriktive."}
+	,{"name":"Æg, 6 stk","unit":"bakker", "price":20}
+	,{"name":"Æg, 15 stk","unit":"bakker", "price":40}
+	,{"name":"Æg, 30 stk","unit":"bakker", "price":75}
+	,{"group":"Kornprodukter"}
+	,{"name":"Ølandshvede, 1 kg","unit":"poser", "price":22}
+	,{"name":"Fint Ølandshvede, 1 kg","unit":"poser", "price":22}
+	,{"name":"Vårrug - knækkede rugkerner, 1 kg","unit":"poser", "price":22}
+	,{"name":"Ølandsperler, 1 kg","unit":"poser", "price":22}
+	,{"name":"Jomfrubygperler, 1 kg","unit":"poser", "price":22}
+	,{"name":"Svedjerug, 1 kg","unit":"poser", "price":22}
+	,{"name":"Havregryn, 1 kg","unit":"poser", "price":22}
+     ]
+    }
 ];
