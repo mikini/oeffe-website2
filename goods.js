3afde12be9f90384534a23cc6d28632729ca0b60
Mikkel Kirkgaard Nielsen <memb_github@mikini.dk>
2016-11-18 17:47:50 +0100

    Add goods for week 48 as a duplicate of week 44 & 46.

--- goods.js
+++ goods.js
@@ -90,4 +90,30 @@ var goods=[
 	,{"name":"Havregryn, 1 kg","unit":"poser", "price":22}
      ]
     }
+    ,{"week":48,
+     "items":[
+	 {"group":"Grøntsager"}
+	,{"name":"Kartofler","unit":"kg.", "price":9}
+	,{"name":"Gulerødder","unit":"kg.", "price":9}
+	,{"name":"Pastinakker","unit":"kg.", "price":9}
+	,{"name":"Løg","unit":"3 stk.", "price":9}
+	,{"name":"Rødbeder","unit":"kg.", "price":9}
+	,{"name":"Gule rødbeder","unit":"3 stk.", "price":9}
+	,{"name":"Stribede rødbeder","unit":"3 stk.", "price":9}
+	,{"name":"Persille","unit":"bundt.", "price":10}
+	,{"name":"Græskar","unit":"stk.", "price":30}
+        ,{"group":"Æg", "description":"Æggene er størrelse M/L, er gennemlyste og datostemplede som reglerne foreskriver ved detailsalg. Køber man som privat direkte fra producenten er reglerne mindre restriktive."}
+	,{"name":"Æg, 6 stk","unit":"bakker", "price":20}
+	,{"name":"Æg, 15 stk","unit":"bakker", "price":40}
+	,{"name":"Æg, 30 stk","unit":"bakker", "price":75}
+        ,{"group":"Kornprodukter"}//, "description":"Økologiske, biodynamiske og Demeter-mærkede kornpodukter fra <a href='http://www.melfraforum.dk/'>Mel fra Forum</a>."}
+	,{"name":"Ølandshvede, 1 kg","unit":"poser", "price":22}
+	,{"name":"Fint Ølandshvede, 1 kg","unit":"poser", "price":22}
+	,{"name":"Vårrug - knækkede rugkerner, 1 kg","unit":"poser", "price":22}
+	,{"name":"Ølandsperler, 1 kg","unit":"poser", "price":22}
+	,{"name":"Jomfrubygperler, 1 kg","unit":"poser", "price":22}
+	,{"name":"Svedjerug, 1 kg","unit":"poser", "price":22}
+	,{"name":"Havregryn, 1 kg","unit":"poser", "price":22}
+     ]
+    }
 ];
