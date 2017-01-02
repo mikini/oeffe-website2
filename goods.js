87c8b3475ca16844da3f5c6ba3ba87084ce5bc82
Mikkel Kirkgaard Nielsen <memb_github@mikini.dk>
2017-01-02 16:53:37 +0100

    Add year element and goods for week 2017-2
    Modify goods defintion array to include year for all elements.
    Add check of year when deciding which week to use.
    Add goods for week 2017-02.

--- goods.js
+++ goods.js
@@ -1,10 +1,12 @@
 var goods=[
     {"week":38,
+     "year":2016,
      "items":[
 	 {"name":"Grøntsagspose","unit":"poser","field":"grøntpose"}
      ]
     }
     ,{"week":40,
+     "year":2016,
      "items":[
 	 {"name":"Vårrug, 2 kg ","unit":"poser","field":"vaarrug", "price":44}
 	,{"name":"Æg, 6 stk","unit":"bakker","field":"6_æg", "price":20}
@@ -13,6 +15,7 @@ var goods=[
      ]
     }
     ,{"week":42,
+     "year":2016,
      "items":[
 	 {"group":"Grøntsager"}
 	,{"name":"Kartofler","unit":"kg.", "price":9}
@@ -39,6 +42,7 @@ var goods=[
      ]
     }
     ,{"week":44,
+     "year":2016,
      "items":[
 	 {"group":"Grøntsager"}
 	,{"name":"Kartofler","unit":"kg.", "price":9}
@@ -65,6 +69,7 @@ var goods=[
      ]
     }
     ,{"week":46,
+     "year":2016,
      "items":[
 	 {"group":"Grøntsager"}
 	,{"name":"Kartofler","unit":"kg.", "price":9}
@@ -91,6 +96,7 @@ var goods=[
      ]
     }
     ,{"week":48,
+     "year":2016,
      "items":[
 	 {"group":"Grøntsager", "description":"Økologiske og lokale varer fra bl. a. <a href='http://www.vardeaadallam.dk'>Varde Ådal Lam</a>."}
 	,{"name":"Kartofler","unit":"kg.", "price":9}
@@ -119,6 +125,7 @@ var goods=[
      ]
     }
     ,{"week":50,
+     "year":2016,
      "items":[
 	 {"group":"Grøntsager", "description":"Økologiske og lokale varer fra bl. a. <a href='http://www.vardeaadallam.dk'>Varde Ådal Lam</a>."}
 	,{"name":"Kartofler","unit":"kg.", "price":9}
@@ -143,6 +150,7 @@ var goods=[
      ]
     }
     ,{"week":52,
+     "year":2016,
      "items":[
 	 {"group":"Grøntsager", "description":"Økologiske og lokale varer fra bl. a. <a href='http://www.vardeaadallam.dk'>Varde Ådal Lam</a>."}
 	,{"name":"Kartofler","unit":"kg.", "price":9}
@@ -150,6 +158,30 @@ var goods=[
 	,{"name":"Pastinakker","unit":"kg.", "price":9}
 	,{"name":"Løg","unit":"kg.", "price":9}
 	,{"name":"Rødbeder","unit":"kg.", "price":9}
+        ,{"group":"Æg", "description":"Størrelsen er M/L, leveres gennemlyste og datostemplede som reglerne foreskriver ved detailsalg. Køber man som privat direkte fra producenten er reglerne mindre restriktive."}
+	,{"name":"Æg, 6 stk","unit":"bakker", "price":20}
+	,{"name":"Æg, 15 stk","unit":"bakker", "price":40}
+	,{"name":"Æg, 30 stk","unit":"bakker", "price":75}
+        ,{"group":"Kornprodukter", "description":"Økologiske, biodynamiske og Demeter-mærkede kornprodukter fra <a href='http://www.melfraforum.dk/'>Mel fra Forum</a>."}
+	,{"name":"Ølandshvede, 1 kg","unit":"poser", "price":22}
+	,{"name":"Fint Ølandshvede, 1 kg","unit":"poser", "price":22}
+	,{"name":"Vårrug, 1 kg","unit":"poser", "price":22, "description":"knækkede rugkerner"}
+	,{"name":"Ølandsperler, 1 kg","unit":"poser", "price":22}
+	,{"name":"Jomfrubygperler, 1 kg","unit":"poser", "price":22}
+	,{"name":"Svedjerug, 1 kg","unit":"poser", "price":22}
+	,{"name":"Havregryn, 1 kg","unit":"poser", "price":22}
+        ,{"group":"Øvrige lækkerier"}
+	,{"name":"Æblemost, 2,5 liter","unit":"dunke", "price":50, "description":"dejlig stærk med en smule frugtkød, fra Frederik Rahbeks plantage på <a href='http://www.forumlund.dk/'>Forumlund</a>"}
+     ]
+    }
+    ,{"week": 2,
+     "year": 2017,
+     "items":[
+	 {"group":"Grøntsager", "description":"Økologiske og lokale varer fra bl. a. <a href='http://www.vardeaadallam.dk'>Varde Ådal Lam</a>."}
+	,{"name":"Kartofler","unit":"kg.", "price":9}
+	,{"name":"Gulerødder","unit":"kg.", "price":9}
+	,{"name":"Pastinakker","unit":"kg.", "price":9}
+	,{"name":"Løg","unit":"kg.", "price":9}
         ,{"group":"Æg", "description":"Størrelsen er M/L, leveres gennemlyste og datostemplede som reglerne foreskriver ved detailsalg. Køber man som privat direkte fra producenten er reglerne mindre restriktive."}
 	,{"name":"Æg, 6 stk","unit":"bakker", "price":20}
 	,{"name":"Æg, 15 stk","unit":"bakker", "price":40}
