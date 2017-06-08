9b2c542a71bd62d4dde9ae4f86f5664d8fdf88c0
Mikkel Kirkgaard Nielsen <memb_github@mikini.dk>
2017-06-08 02:36:06 +0200

    New day statistics and some adjustments
    * Add accumulation of hits pr. day over last 14 days
    * Separate stat types into different tables with proper headings

--- hitcount.php
+++ hitcount.php
@@ -22,19 +22,56 @@ if (count($_GET) > 0)
     echo $total;
     die();
   }
-
   else if (!is_null($_GET['nocount']))
      $count=false;
   // any other make a plain stat page
   else
   {
-    echo "<html><h1>ØFFE besøgs-tæller</h1><table border=1>";
+    echo "<html><h1>ØFFE besøgs-tæller</h1>";
+
+    echo "<ul>";
     if ($total)
     {
-      echo "<tr><td><b>Totale besøg</b></td><td></td><td align=right>".$total."</td></tr>";
+      echo "<li>Totale besøg: ".$total."</li>";
     }
+    // Get lowest year, month, day and display
+    echo "</ul>";
 
-    echo "<tr><td><b>Dato</b></td><td><b>Time<b></td><td><b>Besøg<b></td></tr>";
+    echo "<h2>Besøg pr. døgn</h2>";
+    echo "<table border=1><th>Døgn tilbage</th><th>Besøg</th></tr>";
+    $time=time();
+    foreach(array(0,1,2,3,4,5,6,7,8,9,10,11,12,13) as $daysback)
+    {
+      $date = getdate($time-60*60*24*$daysback);
+      $where = "WHERE "
+        . "year='"  . $date['year']  . "' AND "
+        . "month='" . $date['mon']   . "' AND "
+        . "day='"   . $date['mday']  . "'";
+      $datecountr = $db->query('SELECT sum(hits) FROM hits '.$where.';');
+      if ($datecountr)
+      {
+        $datecount = $datecountr->fetchArray()[0];
+        echo "<tr><td>".$daysback."</td><td align=right>".$datecount."</td></tr>";
+      }
+    }
+    echo "</table>";
+
+    echo "<h2>Akkumuleret time-statistik</h2>";
+    echo "<table border=1>";
+    echo "<tr><th>Time</th><th>Besøg</th><th>Relativt</th></tr>";
+    foreach(array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23) as $hour)
+    {
+      $result = $db->query("SELECT hour, sum(hits) FROM hits WHERE hour=$hour;");
+      while ( $row = $result->fetchArray(SQLITE3_BOTH) )
+      {
+          echo "<tr><td>".$hour."</td><td align=right>".$row[1]."</td><td align=right>".intval(($row[1]/$total)*100)." %</td></tr>";
+      }
+    }
+    echo "</table>";
+
+    echo "<h2>Besøg pr. time</h2>";
+    echo "<table border=1>";
+    echo "<tr><th>Dato</th><th>Time</th><th>Besøg</th></tr>";
     $result = $db->query('SELECT * FROM hits order by year DESC,month DESC,day DESC,hour DESC;');
     while ( $row = $result->fetchArray(SQLITE3_ASSOC) )
     {
@@ -42,18 +79,6 @@ if (count($_GET) > 0)
     }
     echo "</table>";
 
-    echo "<h1>Akkumuleret time-statistik</h1>";
-    echo "<table border=1>";
-    echo "<tr><td><b>Time</b></td><td><b>Besøg<b></td></tr>";
-    foreach(array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23) as $hour)
-    {
-      $result = $db->query("SELECT hour, sum(hits) FROM hits WHERE hour=$hour;");
-      while ( $row = $result->fetchArray(SQLITE3_BOTH) )
-      {
-        echo "<tr><td>".$row[0]."</td><td align=right>".$row[1]."</td>";
-      }
-    }
-    echo "</table>";
     echo "</html>";
     die();
   }
