8a60c68646689ded65a96d114c578a973d880396
Mikkel Kirkgaard Nielsen <miki@vestergaardcompany.com>
2017-06-14 22:57:16 +0200

    Add infrastructure for images of goods, generated from xcf source.

---
+++ goods_img_gen.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+# convert and scale xcf into web friendly jpg
+
+for i in img/goods/*.xcf; do
+  echo Processing $i
+  for s in 160 300 600; do
+    convert ${i} -resize ${s}x ${i%.xcf}_${s}.jpg
+  done
+done
