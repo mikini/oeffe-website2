8edcbcd394c070181e327ea9ccbef34f43e03877
Mikkel Kirkgaard Nielsen <memb_github@mikini.dk>
2017-06-14 23:41:01 +0200

    Use GraphicsMagick instead of ImageMagick.

--- goods_img_gen.sh
+++ goods_img_gen.sh
@@ -4,6 +4,6 @@
 for i in img/goods/*.xcf; do
   echo Processing $i
   for s in 160 300 600; do
-    convert ${i} -resize ${s}x ${i%.xcf}_${s}.jpg
+    gm convert ${i} -resize ${s}x ${i%.xcf}_${s}.jpg
   done
 done
